8c47f32568340033faa5dcd2e5dc04e0fa12d432
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-15 00:11:07 +0100

    built the first round of the ft_2D_presentastion

---
+++ app/build_2D_lattice.f90
@@ -0,0 +1,19 @@
+program fft_func_attempt
+    !use trialpkg, only: say_hello
+    use, intrinsic :: iso_c_binding
+    use fft_utils1
+    use plot_utils
+    use vec_utils
+    implicit none
+    
+    !First, you build the dataset
+    real(real64), dimension(:), allocatable :: X, Y
+    real(real64), allocatable :: M(:,:,:)
+    X = seq(-10.0_real64, 10.0_real64, 10**4)
+    !Y = sin(30*X) + cos(70*X) + sin(75*X) + sin(60*X + 2*asin(1.0)/3) + cos(110*X + 2*acos(-1.0)/3)
+    Y = cos(30*X)
+    M = fft_1D(X, Y)
+    print *, "fft calculation over"
+    call plot_gnu(M(:,1,1), M(:,2,1)) !The amplitude spectra
+    call plot_gnu(M(:,1,2), M(:,2,2)) !The phase spectra
+end program fft_func_attempt
--- test/test_fft_2D.f90
+++ test/test_fft_2D.f90
@@ -10,13 +10,15 @@ program test_fft2D_function
   real(real64), allocatable :: res(:,:,:), dumm(:,:), final(:,:,:), tens_XY(:,:,:)
   integer :: n, m
   n = 500
-  m = 600
-  X = seq(st=-4.0_real64*pi, en=4.0_real64, len=n)
-  Y = seq(st=-3.0_real64*pi, en=-3.0_real64*pi, len=m)
+  m = 500
+  X = seq(st=-5.0_real64*pi, en=5.0_real64, len=n)
+  Y = seq(st=-5.0_real64*pi, en=5.0_real64*pi, len=m)
   tens_XY = grid_2(X,Y)
   allocate(res(m, n, 3))
   res(:,:,2) = tens_XY(:,:,1)
   res(:,:,3) = tens_XY(:,:,2)
-  res(:,:,1) = sin(-3.0_real64*res(:,:,2)+4.0_real64*res(:,:,3))
+  res(:,:,1) = sin(-1.0_real64*res(:,:,2)+ 0.5_real64*res(:,:,3))
+  call MatrixWrite(res(:,:,1), 'lattice')
   final = fft_2D(res)
+  call MatrixWrite(final(:,:,1), 'ft_lattice')
 end program test_fft2D_function
