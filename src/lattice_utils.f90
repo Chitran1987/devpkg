81fa5b27f15099a28705bd06786aa3a8ad981d4b
Chitran Ghosal <ghosal.chitran@gmail.com>
2025-11-25 17:18:50 +0100

    Introduced the rect_lattice.f90 case

--- src/lattice_utils.f90
+++ src/lattice_utils.f90
@@ -62,7 +62,51 @@ function square_latt_sb(X, Y, R_latt, A, sig) result(tens)
 end function square_latt_sb
 
 !Define a rectangular lattice
+function rect_latt_sb(X, Y, R_latt_x, R_latt_y, A, sig) result(tens)
+  real(real64) :: X(:), Y(:), R_latt_x, R_latt_y !Input
+  real(real64) :: A, sig !Gaussian amplitude and sdev
+  real(real64) :: tens(size(Y),size(X),3) !Output
+  real(real64), allocatable :: tens_XY_00(:,:,:) !The same as tens with the X-Y grid in tens(:,:,:) set to X=0, Y=0, at the leftmost corner
+  integer :: m_x, m_y
+  integer :: i, j, k, cnt, dmp
+  real(real64) :: xmin, xmax, ymin, ymax, gauss_dummy(size(Y),size(X)) ! gauss_dummy(size(Y),size(X),3)
+  real(real64), allocatable  :: X_sh(:), Y_sh(:) !The shifted X and Y vectors
+  real(real64) , allocatable :: pos(:,:)
 
+  !error check
+  if ( R_latt_x > maxval(X) - minval(X)  ) then
+    error stop "R_latt too large for array"
+  end if
+  if ( R_latt_y > maxval(Y) - minval(Y) ) then
+    error stop "R_latt too large for array"
+  end if
+
+  !allocate size of tens_XY_00 and build its X and Y and then shift the leftmost corner to (0,0)
+  allocate(tens_XY_00(size(Y), size(X), 3))
+  tens(:,:,2:3) = grid_2(X, Y)
+  tens_XY_00(:,:,2:3) = tens(:,:,2:3)
+  tens_XY_00(:,:,2) = tens_XY_00(:,:,2) - minval(X) !shift X to 0
+  tens_XY_00(:,:,3) = tens_XY_00(:,:,3) - minval(Y) !shift y to 0
+  tens_XY_00(:,:,1) = 0.0_real64
+
+  !figure out the size of the loop and then the vector
+  m_x = floor((maxval(X) - minval(X))/R_latt_x)
+  m_y = floor((maxval(Y) - minval(Y))/R_latt_y)
+  allocate(pos((m_x+1)*(m_y+1), 2)) !Debug vector. Remove after debug
+
+  !Run the loop
+  X_sh = X - minval(X)
+  Y_sh = Y - minval(Y)
+  cnt = 1 !New indexing integer
+  do k = 0, m_x
+    do j = 0, m_y
+      gauss_dummy = gauss_2D_nocorr_core(X=tens_XY_00(:,:,2), Y=tens_XY_00(:,:,3), A = 1.0_real64, x0 = k*R_latt_x, y0 = j*R_latt_y, sig_x = sig, sig_y = sig)
+      tens_XY_00(:,:,1) = tens_XY_00(:,:,1) + gauss_dummy
+    end do
+  end do
+
+  tens(:,:,1) = tens_XY_00(:,:,1)
+end function rect_latt_sb
 
 !Define a hexagonal lattice
 
---
+++ test/rect_lattice_build.f90
@@ -0,0 +1,27 @@
+program rect_lattice_build
+    use iso_fortran_env, only: real64
+    use vec_utils
+    use stat_utils
+    use plot_utils
+    use calculus_utils
+    use fft_utils1
+    use lattice_utils
+    implicit none
+    real(real64), allocatable :: X(:), Y(:), latt(:,:,:), grid(:,:,:) !Lattice variables
+    real(real64), allocatable :: f_latt(:,:,:)
+    !build the square lattice
+    X = seq(st=-10.0_real64, en=10.0_real64, len=500)
+    Y = X
+    grid = grid_2(X, Y)
+    allocate(latt(size(Y), size(X), 3))
+    !latt(:,:,2) = grid(:,:,1)
+    !latt(:,:,3) = grid(:,:,2)    
+    !latt(:,:,1) = sin(3*latt(:,:,2)+4*latt(:,:,3))*sin(4*latt(:,:,2)-3*latt(:,:,3))
+    latt = rect_latt_sb(X = X, Y = Y, R_latt_x = 0.75_real64, R_latt_y = 0.5_real64, A = 1.0_real64, sig = 0.15_real64)
+    call MatrixWrite(M=latt(:,:,1), nam='rect_lattice')
+
+    !build the Fourier Transform
+    f_latt = fft_2D(tens = latt)
+    call MatrixWrite(M=f_latt(:,:,1), nam = 'ft_rect_lattice')
+    
+end program rect_lattice_build
