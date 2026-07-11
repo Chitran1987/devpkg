729ade0077d8be71056311ccf4a45cf848791d14
Chitran Ghosal <ghosal.chitran@gmail.com>
2026-07-11 02:04:33 +0200

    Two functions, int_search() and latt_R_convert() both should be private functions

--- src/lattice_utils.f90
+++ src/lattice_utils.f90
@@ -7,7 +7,43 @@ module lattice_utils
   use plot_utils
   implicit none
   
+  private :: int_search
+
+
 contains
+
+!Private procedure to this module
+!Creates a matrix of integers such that m,n,p,q = -int_range to +int_range
+!Ensures that all combinations of m,n,p,q exist in a matrix where
+!.....1st column are values of m, 2nd col->n, 3rd col->p, 4th col->q
+!5th col -> mq-np
+!Hence, returns a (2*int_range + 1) row, 5 column matrix
+!Used in functions local to module ---> private 
+function int_search(int_range) result(list_mat)
+    integer, intent(in) :: int_range
+    integer :: list_mat((2*int_range+1)**4, 5)
+    integer :: m, n, p, q
+    integer :: dumm, cnt
+    !code
+    m = int_range
+    cnt = 1
+    do m = -int_range, int_range
+        do n = -int_range, int_range
+            do p = -int_range, int_range
+                do q = -int_range, int_range
+                    dumm = m*q - n*p
+                    list_mat(cnt,1) = m
+                    list_mat(cnt, 2) = n
+                    list_mat(cnt, 3) = p
+                    list_mat(cnt, 4) = q
+                    list_mat(cnt, 5) = dumm
+                    cnt = cnt+1
+                end do
+            end do
+        end do
+    end do
+    return
+end function int_search
 
 !Define a square lattice
 function square_latt_sb(X, Y, R_latt, A, sig) result(tens)
@@ -108,10 +144,62 @@ function rect_latt_sb(X, Y, R_latt_x, R_latt_y, A, sig) result(tens)
   tens(:,:,1) = tens_XY_00(:,:,1)
 end function rect_latt_sb
 
+!Converts provided lattices R1 and R2 to 1st quadrant R1 and R2 
+!Returns dta in a matrix M
+!R1 is the 1st column, R2 is the 2nd column
+function latt_R_convert(R1, R2, search_rad) result(col_R1_R2)
+        !Inputs
+        real(real64), intent(in) :: R1(2), R2(2)
+        integer, intent(in) :: search_rad
+        !Outputs
+        real(real64) :: col_R1_R2(2,2)
+        !Internals
+        real(real64) :: latt_mat(2,2), det_latt
+        real(real64), allocatable :: M1(:,:), M2(:,:) 
+        real(real64) :: S1(2), S2(2)
+        integer :: m, n, p, q
+        logical, allocatable :: mask(:)
+        integer :: i, mask_true
+        !Error check
+        latt_mat(:,1) = R1
+        latt_mat(:,2) = R2
+        det_latt = latt_mat(1,1)*latt_mat(2,2) - latt_mat(1,2)*latt_mat(2,1)
+        if ( abs(det_latt) <= 10**(-10) ) then
+            error stop "R1 and R2 are linearly dependent"
+        end if
+        M1 = int_search(int_range = search_rad)
+        mask = abs(M1(:,5)) == 1
+        mask_true = count(mask)
+        allocate(M2(mask_true, 4))
+        M2(:,1) = pack(M1(:,1), mask)
+        M2(:,2) = pack(M1(:,2), mask)
+        M2(:,3) = pack(M1(:,3), mask)
+        M2(:,4) = pack(M1(:,4), mask)
+        do i = 1, size(M2(:,1))
+            !define m, n, p, q
+            m = M2(i,1)
+            n = M2(i,2)
+            p = M2(i,3)
+            q = M2(i,4)
+            S1 = m*R1 + n*R2
+            S2 = p*R1 + q*R2
+            if ( S1(1) >= 0 .and. S1(2) >= 0 .and. S2(1) >= 0 .and. S2(2) >= 0 ) then
+                if ( .not.(S1(1) == 0 .and. S1(2) == 0) .and. .not.(S2(2) ==0 .and. S2(1) == 0)) then
+                    col_R1_R2(:,1) = S1
+                    col_R1_R2(:,2) = S2
+                    return 
+                end if
+            end if
+        end do
+
+end function latt_R_convert
+
+!Define an arbitrary lattice
+
 !Define a hexagonal lattice
 
 !Define a honeycomb lattice
 
-!Define an arbitrary lattice
+
   
 end module lattice_utils
