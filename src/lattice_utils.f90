893734df81df820cf23327e4c75c12d8a56b6634
Chitran Ghosal <ghosal.chitran@gmail.com>
2026-07-31 14:13:05 +0200

    Update lattice_utils

--- src/lattice_utils.f90
+++ src/lattice_utils.f90
@@ -7,7 +7,7 @@ module lattice_utils
   use plot_utils
   implicit none
   
-  private :: int_search, latt_R_convert
+  !private :: int_search, latt_R_convert
 
 
 contains
@@ -95,6 +95,131 @@ function latt_R_convert(R1, R2, search_rad) result(col_R1_R2)
         end do
         error stop "No first-quadrant primitive basis found. Increase search_rad or provide a manually reoriented unit cell."
 end function latt_R_convert
+
+!create a latt_pnt_lst() function
+!The function should return a list of lattice points of m*R1+n*R2 type in the X-Y space
+!shft = 0 means that once the lattice points are created
+    !!There shouldat least be one lattice point present at the bottom left corner (X_min, Y_min)
+    !!shft /= 0 means one latt point at (X_min+shft(1), Y_min+shft(2))
+function latt_pnt_lst(X, Y, R1, R2, shft) result(lst_mat)
+    real(real64) :: R1(2), R2(2), shft(2) !Inputs
+    real(real64), intent(in) :: X(:), Y(:)
+    real(real64), allocatable :: lst_mat(:,:) !Outputs
+    real(real64) :: R1R2_mat(2,2), X1(size(X)), Y1(size(Y)) !Internals
+    real(real64) :: R1_int(2), R2_int(2), lenX, lenY, t1, t2, p(2) !Internals
+    real(real64) :: V_low(2), V_up(2) !Internals
+    real(real64) :: distX, distY, dist_R1, dist_R2, vec(2), c1, c2, min_vec(2)  !Internals
+    real(real64), allocatable :: M1(:,:), Mx(:,:), My(:,:)  !Internals
+    real(real64), allocatable :: dis_small(:) !Internals
+    integer :: r, s, cnt, i, j, k, shft_idx !Internals
+    logical, allocatable :: mask_X(:), mask_Y(:)  !Internals
+
+    !code----------------------------------------------------------------
+    !!!swap R1 and R2 if necessary----------
+    X1 = X - minval(X)
+    Y1 = Y - minval(Y)
+    !Get R1 and R2 into first quadrants
+    R1R2_mat = latt_R_convert(R1, R2, search_rad=5)
+    R1_int = R1R2_mat(:,1)
+    R2_int = R1R2_mat(:,2)
+    !swap R1_int and R2_int if necessary
+    if ( atan(R1_int(2)/R1_int(1)) > atan(R2_int(2)/R2_int(1)) ) then
+        R1R2_mat(:,1) = R2_int
+        R1R2_mat(:,2) = R1_int
+    end if
+    R1 = R1R2_mat(:,1)
+    R2 = R1R2_mat(:,2)
+    !!!Calculate the skewed shape--------------
+    lenX = maxval(X1) - minval(X1)
+    lenY = maxval(Y1) - minval(Y1)
+    t1 = R1(2)/R1(1)
+    t2 = R2(2)/R2(1)
+    c1 = -(1.5_real64*lenX*t1 + 0.5_real64*lenY)
+    c2 =  0.5_real64*lenX*t2 + 1.5_real64*lenY
+    p(1) = -(c2 - c1)/(t2 - t1)
+    p(2) = (t2*c1 - t1*c2)/(t2 - t1)
+    V_low = [1.5_real64*lenX, -0.5_real64*lenY]
+    V_up = [-0.5_real64*lenX, 1.5_real64*lenY]
+    distX = sqrt(sum((p - V_low)*(p - V_low)))
+    distY = sqrt(sum((p - V_up)*(p - V_up)))
+    dist_R1 = sqrt( sum(R1*R1) )
+    dist_R2 = sqrt( sum(R2*R2) )
+    r = ceiling(distX/dist_R1)
+    s = ceiling(distY/dist_R2)
+    allocate(M1((r+1)*(s+1),2))
+    vec = p
+    cnt = 1
+    !!! Populate the skewed shape-------------------------
+    do i = 0, r
+        do j = 0, s
+            vec = p + i*R1 + j*R2
+            M1(cnt,:) = vec
+            cnt = cnt + 1
+        end do
+    end do
+    !!!Snip the skewed shape to a large rectangle -----------------------------
+    mask_X = ( M1(:,1)>=-0.5_real64*lenX ) .and. ( M1(:,1) <= 1.5_real64*lenX )
+    allocate(Mx(count(mask_X),2))
+    Mx(:,1) = pack(M1(:,1), mask_X)
+    Mx(:,2) = pack(M1(:,2), mask_X)
+    !Now Mx is succesfully populated
+    !redefine the mask
+    mask_Y = (Mx(:,2)>=-0.5_real64*lenY) .and. (Mx(:,2) <= 1.5_real64*lenY)
+    allocate(My(count(mask_Y),2))
+    My(:,1) = pack(Mx(:,1), mask_Y)
+    My(:,2) = pack(Mx(:,2), mask_Y)
+    !Now that My is the final list first stage before shifting
+    lst_mat = My
+    !!!Shift the list of point accordingly 
+    !calculate the distance of each point 
+    !from the lower left corner of the image
+    !Then take the pt corresponding to the least distance
+    !shift the entire image by the shift of the corresponding point 
+    !to the co-ordinates of the lower-left image corner
+    dis_small = sqrt(lst_mat(:,1)**2 + lst_mat(:,2)**2)
+    shft_idx = minloc(dis_small, dim=1)
+    min_vec = lst_mat(shft_idx,:)
+    do k = 1, size(lst_mat(:,1))
+        lst_mat(k,:) = lst_mat(k,:) - min_vec
+    end do
+    !Then shift the entire bunch by shft
+    !Then shift it by the actual upper and lower corners of X and Y
+    do k = 1, size(lst_mat(:,1))
+        lst_mat(k,:) = lst_mat(k,:) + shft + [minval(X), minval(Y)] !shift by shft
+    end do
+    !Then shift it by 
+    !Then return
+    return
+end function latt_pnt_lst
+
+!Populate a lattice given The X, Y and lattice point list
+!Returns a 3 dimensional tensor for it
+function populate_latt(X, Y, A, sig, list) result(tens)
+
+    real(real64), intent(in) :: X(:), Y(:)
+    real(real64), intent(in) :: list(:,:)
+    real(real64), intent(in) :: A, sig
+
+    real(real64) :: tens(size(Y), size(X), 3)
+    real(real64) :: gauss_dumm(size(Y), size(X))
+    real(real64) :: Y_local(size(Y))
+
+    integer :: i
+
+    ! grid_2 reverses its Y argument in place,
+    ! so give it a local copy.
+    Y_local = Y
+
+    tens(:,:,2:3) = grid_2(X, Y_local)
+    tens(:,:,1) = 0.0_real64
+
+    do i = 1, size(list, 1)
+        gauss_dumm = gauss_2D_nocorr_core( X = tens(:,:,2), Y = tens(:,:,3), A = A, x0 = list(i,1), y0 = list(i,2), sig_x = sig, sig_y = sig)
+        tens(:,:,1) = tens(:,:,1) + gauss_dumm
+    end do
+    return
+end function populate_latt
+
 
 !Define a square lattice
 !single atomic basis(sb)
@@ -198,86 +323,15 @@ function rect_latt_sb(X, Y, R_latt_x, R_latt_y, A, sig) result(tens)
 end function rect_latt_sb
 
 !Define an arbitrary lattice
-function latt(X, Y, R1, R2, A, sig) result(tens)
-    real(real64) :: X(:), Y(:), R1(2), R2(2), A, sig !Inputs
+function latt_arb(X, Y, R1, R2, A, sig, shft) result(tens)
+    real(real64) :: X(:), Y(:), R1(2), R2(2), A, sig, shft(2) !Inputs
     real(real64) :: tens(size(Y), size(X), 3) !Outputs
     !Internal declarations
-    real(real64), allocatable :: X1(:), Y1(:) !Internals
-    real(real64) :: R1_int(2), R2_int(2), lenX, lenY, M(2,2) !Internals
-    real(real64) :: t1, t2 !Tan(\theta) values for R1_int and R2_int !Internals
-    real(real64) :: p(2) !The point from which to start my lattice calculation
-    real(real64) :: distX, distY !The distances from p to (lenX,0) and p to (0, lenY)
-    real(real64) :: dist_R1, dist_R2 !The magnitudes of R1 and R2
-    real(real64), allocatable :: M1(:,:) !The first internal matrix for calculation !Internal
-    real(real64), allocatable :: Mx(:,:), My(:,:) !The matrixes built as a result of subsetting M1
-    logical, allocatable :: mask(:) !The mask
-    real(real64), allocatable :: final_list(:,:) !The final list of co-ordinates which need to populate the image
-    integer :: r, s !M1 === M1(m*n,2)
-    integer :: i,j,k !Loop indexes
-    integer :: cnt !Counter
-    real(real64) :: vec(2) !The iterative vector
-    real(real64) :: gauss_dummy(size(Y), size(X)) !The dummy gauss for iteration
-    !code_0
-    X1 = X - minval(X)
-    Y1 = Y - minval(Y)
-    !Get R1 and R2 into first quadrants
-    M = latt_R_convert(R1, R2, search_rad=5)
-    R1_int = M(:,1)
-    R2_int = M(:,2)
-    !swap R1_int and R2_int if necessary
-    if ( atan(R1_int(2)/R1_int(1)) > atan(R2_int(2)/R2_int(1)) ) then
-        M(:,1) = R2_int
-        M(:,2) = R1_int
-    end if
-    R1 = M(:,1)
-    R2 = M(:,2)
-    !code_1
-    lenX = maxval(X1) - minval(X1)
-    lenY = maxval(Y1) - minval(Y1)
-    t1 = R1(2)/R1(1)
-    t2 = R2(2)/R2(1)
-    p(1) = (lenY + lenX*t1)/(t1-t2)
-    p(2) = (lenY*t1 + lenX*t1*t2)/(t1 - t2)
-    distX = sqrt( sum((p - [lenX, 0.0_real64])*(p - [lenX, 0.0_real64])) )
-    distY = sqrt( sum((p - [0.0_real64, lenY])*(p - [0.0_real64, lenY])) )
-    dist_R1 = sqrt( sum(R1*R1) )
-    dist_R2 = sqrt( sum(R2*R2) )
-    r = ceiling(distX/dist_R1)
-    s = ceiling(distY/dist_R2)
-    allocate(M1((r+1)*(s+1),2))
-    vec = p
-    cnt = 1
-    do i = 0, r
-        do j = 0, s
-            vec = p + i*R1 + j*R2
-            M1(cnt,:) = vec
-            cnt = cnt + 1
-        end do
-    end do
-    !Now, the matrix M1 is succesfully populated
-    mask = ( M1(:,1)>=0.0_real64 ) .and. ( M1(:,1) <= lenX )
-    allocate(Mx(count(mask),2))
-    Mx(:,1) = pack(M1(:,1), mask)
-    Mx(:,2) = pack(M1(:,2), mask)
-    !Now Mx is succesfully populated
-    !redefine the mask
-    mask = (Mx(:,2)>=0.0_real64) .and. (Mx(:,2) <= lenY)
-    allocate(My(count(mask),2))
-    My(:,1) = pack(Mx(:,1), mask)
-    My(:,2) = pack(Mx(:,2), mask)
-    !Now that My is the final list
-    final_list = My
-    !Now create the tensor
-    tens(:,:,2:3) = grid_2(X=X1, Y=Y1)
-    tens(:,:,1) = 0.0_real64
-    do k = 1, size(final_list(:,1))
-        gauss_dummy = gauss_2D_nocorr_core(X=tens(:,:,2), Y=tens(:,:,3), A = A, x0 = final_list(k,1), y0 = final_list(k,2), sig_x = sig, sig_y = sig)
-        tens(:,:,1) = tens(:,:,1) + gauss_dummy
-    end do
-    tens(:,:,2) = tens(:,:,2) + minval(X)
-    tens(:,:,3) = tens(:,:,3) + minval(Y)
+    real(real64), allocatable :: lst(:,:)
+    lst = latt_pnt_lst(X=X, Y=Y, R1=R1, R2=R2, shft=shft)
+    tens = populate_latt(X=X, Y=Y, A=A, sig=sig, list=lst)
     return
-end function latt
+end function latt_arb
 
 
 !Define a hexagonal lattice
@@ -289,7 +343,7 @@ function hex_latt_sb(X, Y, R_latt, A, sig) result(tens)
     !code
     R1 = [R_latt, 0.0_real64]
     R2 = [R_latt*cos(pi/3), R_latt*sin(pi/3)]
-    tens = latt(X, Y, R1, R2, A, sig)
+    tens = latt_arb(X=X, Y=Y, R1=R1, R2=R2, A=A, sig=sig, shft=[0.0_real64, 0.0_real64])
     return
 end function hex_latt_sb
 
--- test/rect_latt_XY_diff.f90
+++ test/rect_latt_XY_diff.f90
@@ -7,21 +7,21 @@ program rect_lattice_XY_diff
     use fft_utils1
     use lattice_utils
     implicit none
-    real(real64), allocatable :: X(:), Y(:), latt(:,:,:), grid(:,:,:) !Lattice variables
+    real(real64), allocatable :: X(:), Y(:), latt1(:,:,:), grid(:,:,:) !Lattice variables
     real(real64), allocatable :: f_latt(:,:,:)
     !build the square lattice
     X = seq(st=-10.0_real64, en=10.0_real64, len=1000)
     Y = seq(st = -8.0_real64, en = 8.0_real64, len = 800)
     !grid = grid_2(X, Y)
-    allocate(latt(size(Y), size(X), 3))
+    allocate(latt1(size(Y), size(X), 3))
     !latt(:,:,2) = grid(:,:,1)
     !latt(:,:,3) = grid(:,:,2)    
     !latt(:,:,1) = sin(3*latt(:,:,2)+4*latt(:,:,3))*sin(4*latt(:,:,2)-3*latt(:,:,3))
-    latt = rect_latt_sb(X = X, Y = Y, R_latt_x = 0.75_real64, R_latt_y = 0.5_real64, A = 1.0_real64, sig = 0.15_real64)
-    call MatrixWrite(M=latt(:,:,1), nam='rect_lattice_XY_diff')
+    latt1 = rect_latt_sb(X = X, Y = Y, R_latt_x = 0.75_real64, R_latt_y = 0.5_real64, A = 1.0_real64, sig = 0.15_real64)
+    call MatrixWrite(M=latt1(:,:,1), nam='rect_lattice_XY_diff')
 
     !build the Fourier Transform
-    f_latt = fft_2D(tens = latt, sampling_del=0.1_real64)
+    f_latt = fft_2D(tens = latt1, sampling_del=0.1_real64)
     call MatrixWrite(M=f_latt(:,:,1), nam = 'ft_rect_lattice_XY_diff')
     
 end program rect_lattice_XY_diff
